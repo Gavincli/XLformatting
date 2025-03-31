651e46b312f76289c8c76b2049d66daaf75761c3
mshep8 <marycatherineshepherd@gmail.com>
2025-03-31 16:32:53 -0600

    Update XLformatting.py

--- XLformatting.py
+++ XLformatting.py
@@ -1,3 +1,11 @@
-#group names:
+# Mary Catherine Shepherd, Sam Jenson, Gavin Clifton, Ben Funk, Thomas Apke
 
 pass
+
+# formats each column with bold font and proper size
+for col in ['A', 'B', 'C', 'D', 'F', 'G']:
+    sheet[f'{col}1'].font = Font(bold = True)
+    sheet.column_dimensions[col].width = len(sheet[f'{col}1'].value) + 5
+
+# Saves the new file
+Poorly_Organized_Data_1.save('formatted_grades.xlsx')
