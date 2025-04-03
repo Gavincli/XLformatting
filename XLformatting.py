1425b46edac677ee8aefc0138009da6cd4c3a42a
sammjen <samjen@byu.edu>
2025-04-02 20:23:22 -0600

    Finishing touches

--- XLformatting.py
+++ XLformatting.py
@@ -38,8 +38,19 @@ for row in currentSheet.iter_rows(min_row=rowNum, values_only=True):
     myWorkbook[classTitle]['B' + str(printRow)] = lstStudInfo[1]
     myWorkbook[classTitle]['C' + str(printRow)] = lstStudInfo[2]
     myWorkbook[classTitle]['D' + str(printRow)] = lstStudInfo[3]
+    myWorkbook[classTitle]['F1'] = 'Summary Statistics'
+    myWorkbook[classTitle]['F2'] = 'Highest Grade'
+    myWorkbook[classTitle]['F3'] = 'Lowest Grade'
+    myWorkbook[classTitle]['F4'] = 'Mean Grade'
+    myWorkbook[classTitle]['F5'] = 'Median Grade'
+    myWorkbook[classTitle]['F6'] = 'Number of Students'
+    myWorkbook[classTitle]['G1'] = 'Value'
+    myWorkbook[classTitle]['G2'] = '=MAX(D2:D100)'
+    myWorkbook[classTitle]['G3'] = '=MIN(D2:D100)'
+    myWorkbook[classTitle]['G4'] = '=AVERAGE(D2:D100)'
+    myWorkbook[classTitle]['G5'] = '=MEDIAN(D2:D100)'
+    myWorkbook[classTitle]['G6'] = '=COUNT(D2:D100)'
 
-    
 
 
 
@@ -49,9 +60,16 @@ for worksheet in myWorkbook.worksheets:
     worksheet.auto_filter.ref = f"A1:D{max_row}"
 
 # formats each column with bold font and proper size
-for col in ['A', 'B', 'C', 'D', 'F', 'G']:
-    currentSheet[f'{col}1'].font = Font(bold = True)
-    currentSheet.column_dimensions[col].width = len(str(currentSheet[f'{col}1'].value)) + 5
+# Corrected formatting section - applies to ALL worksheets
+for worksheet in myWorkbook.worksheets:
+    # Format headers
+    for col in ['A', 'B', 'C', 'D', 'F', 'G']:
+        header_cell = worksheet[f'{col}1']
+        header_cell.font = Font(bold=True)
+        # Get length of header text or 10 as default
+        header_length = len(str(header_cell.value)) if header_cell.value else 10
+        worksheet.column_dimensions[col].width = header_length + 5
+
 
 # Saves the new file
 myWorkbook.save('formatted_grades.xlsx')
