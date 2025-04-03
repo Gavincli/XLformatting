411bae9b3ecde354d6a071d54751e7ddb486214e
sammjen <samjen@byu.edu>
2025-04-02 20:08:50 -0600

    Update XLformatting.py

--- XLformatting.py
+++ XLformatting.py
@@ -7,7 +7,7 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 
 #insert path to the file!!
-myWorkbook = openpyxl.load_workbook()
+myWorkbook = openpyxl.load_workbook("/Users/samjenson/Desktop/poorlyorganized.xlsx")
 # setting up the worksheet to be the active sheet in our file
 currentSheet = myWorkbook.active
 
@@ -23,8 +23,8 @@ for row in currentSheet.iter_rows(min_row=rowNum, values_only=True):
     lstStudInfo.append(row[2])
 
     if classTitle not in myWorkbook.sheetnames:
+        worksheet = myWorkbook.create_sheet(title=classTitle)
         printRow = 1
-        myWorkbook.create_sheet(title=classTitle)
         myWorkbook[classTitle]['A1'] = 'First Name'
         myWorkbook[classTitle]['B1'] = 'Last Name'
         myWorkbook[classTitle]['C1'] = 'Student ID'
@@ -33,13 +33,13 @@ for row in currentSheet.iter_rows(min_row=rowNum, values_only=True):
     #move down a row per iteration
     printRow += 1
     #print values on the new sheet as long as the classTitle matches
-    #myWorkbook[classTitle]['A' + str(printRow)].append(lstStudInfo)
+    # myWorkbook[classTitle]['A' + str(printRow)].append(lstStudInfo)
     myWorkbook[classTitle]['A' + str(printRow)] = lstStudInfo[0]
     myWorkbook[classTitle]['B' + str(printRow)] = lstStudInfo[1]
     myWorkbook[classTitle]['C' + str(printRow)] = lstStudInfo[2]
     myWorkbook[classTitle]['D' + str(printRow)] = lstStudInfo[3]
 
-        worksheet = myWorkbook.create_sheet(title=classTitle)
+    
 
 
 
@@ -50,8 +50,8 @@ for worksheet in myWorkbook.worksheets:
 
 # formats each column with bold font and proper size
 for col in ['A', 'B', 'C', 'D', 'F', 'G']:
-    sheet[f'{col}1'].font = Font(bold = True)
-    sheet.column_dimensions[col].width = len(sheet[f'{col}1'].value) + 5
+    currentSheet[f'{col}1'].font = Font(bold = True)
+    currentSheet.column_dimensions[col].width = len(str(currentSheet[f'{col}1'].value)) + 5
 
 # Saves the new file
 myWorkbook.save('formatted_grades.xlsx')
